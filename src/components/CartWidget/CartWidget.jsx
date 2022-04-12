787caf5b44f9f2d290f8a95966a43eed4699ffa0
franco fazzio <francofazzio09@hotmail.com>
2022-04-12 15:05:45 -0300

    context

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,98 @@
+import { useState, useContext } from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import CartContext from '../../context/CartContext';
 
 const CartWidget = () => {
+    const { cartProducts } = useContext(CartContext)
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+    console.log("cartProducts: ", cartProducts)
+
     return (
-        <div>
-        <img
-        src={'./carrito.jpg'}
-        className="carrito-header"/>
+        <div className='cart-button'>
+            <ShoppingCartIcon 
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+            />
+            <p>{cartProducts.length}</p>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                className='cart-modal'
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                elevation: 0,
+                sx: {
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 1.5,
+                    '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                    },
+                    '&:before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+                    zIndex: 0,
+                    },
+                    },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                <p>Carrito de Compras</p>
+                <Divider />
+                {cartProducts.map( (cartProduct) => {
+                    return(
+                        <MenuItem className='item-cart-modal' key={cartProduct.id}>
+                            <div className='item-cart-modal__img'>
+                                <img src={`./${cartProduct.image}`} /> 
+                            </div>
+                            <div className='item-cart-modal__info'>
+                                <p>{cartProduct.title}</p>
+                                <span>$ {cartProduct.price}</span>
+                            </div>
+                            <div className='item-cart-modal__action'>
+                                <DeleteIcon />
+                            </div>
+                        </MenuItem>
+                    )
+                })}
+                
+                <Divider />
+                <div className='footer-modal-cart'>
+                    <Button className="btn-custom"><Link to="/cart">Iniciar la compra</Link></Button>
+                </div>
+            </Menu>
         </div>
-    
     )
 }
+
 export default CartWidget
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,79 +1,100 @@
 import { useState, useContext } from 'react';
 import Button from '@mui/material/Button';
-import CartWidget from '../CartWidget/CartWidget';
-import Card from '../Card/Card';
+import { Link } from 'react-router-dom'
 import './NavBar.css'
-import {Link} from 'react-router-dom'
+import CartWidget from '../CartWidget/CartWidget'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import ThemeSwitch from './ThemeSwitch'
 import ThemeContext from '../../context/ThemeContext';
 
-function NavBar(props){
-  const { lightTheme, handleTheme } = useContext(ThemeContext)
-  
-  
-  console.log("Light state " , lightTheme)
+function NavBar(props) {
+    const { lightTheme, handleTheme } = useContext(ThemeContext)
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    console.log("Light state: " , lightTheme)
     const pages = [
     {
         title:'Home',
         url: '/'
     },
+ 
     {
-        title:'Nosotros',
+        title:'Productos',
         url: '/nosotros'
     }, 
     {
-        title: 'Productos',
-        url: '/productos'
+        title:'Nosotros',
+        url: '/nosotros'
     },
     {
         title: 'Contacto',
         url: '/contacto'
     }]
-    
-  
-  
-  return(
+    return(
+        //JSX
+        //
         <header className={`main-header ${lightTheme ? ' light-mode' : ''}`}> 
-        <div className='container-logo'>
-            {lightTheme ? (
-                <img src="logo1234.jpeg" className="img-header"/>
-            ) : (
-            <img src="../logo1234.jpeg" className="img-header"/>
-            ) }
+            <div className='container-logo'>
+                {lightTheme ? (
+                    <img src="../logo1234.jpeg" className="img-header"/>
+                ) : (
+                    <img src="../logo1234.jpeg" className="img-header"/>
+                ) }
             </div>
-        <ul className='navbar'>
-                    <li>
-                        <Button className="custom-btn" variant="contained">
-                            <Link to={'/ellas'}>Ellas</Link>
-                        </Button>     
-                    </li>
-                    <li>
-                        <Button className="custom-btn" variant="contained">
-                            <Link to={'/ellos'}>Ellos</Link>
-                        </Button>     
-                    </li>
-                    <li>
-                        <Button className="custom-btn" variant="contained">
-                            <Link to={'/escolares'}>Escolares</Link>
-                        </Button>     
-                    </li>
-                    <button onClick={() => handleTheme()}>Handle Theme</button>
-        {pages.map((page) => {
-            return(       
-             <li>
-               <Button className="custom-btn" variant="contained">
-                 <Link to={page.url}>{page.title}</Link>
-                  </Button>
-             </li>
-            )
-          })}
-        </ul>
-        <ThemeSwitch />
-        <CartWidget />
-      </header>
-      
+            <ul className='navbar'> 
+                {pages.map((page) => {
+                    return(
+                        page.title === 'Productos' ? (
+                        <li>
+                            <Button 
+                                aria-controls={open ? 'basic-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick}
+                            >{page.title    }</Button>
+                            <Menu
+                                id="basic-menu"
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                MenuListProps={{
+                                'aria-labelledby': 'basic-button',
+                                }}
+                            >
+                                <MenuItem onClick={handleClose}>
+                                    <Link to={'/ellas'}>Ellas</Link>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <Link to={'/ellos'}>Ellos</Link>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <Link to={'/escolares'}>Escolares</Link>
+                                </MenuItem>
+                            </Menu> 
+                        </li>
+                        ) : (
+                        <li>
+                            <Button className="custom-btn" variant="contained">
+                                <Link to={page.url}>{page.title}</Link>
+                            </Button>
+                        </li>
+                        )
+                        
+                    )
+                })}
+            </ul>
+            <ThemeSwitch />
+            <CartWidget />
+        </header>
     )
-    
 }
 
 export default NavBar
