3e86f89f0d01741ca4288ae06b2b2ed250c078b3
franco fazzio <francofazzio09@hotmail.com>
2022-04-21 22:38:47 -0300

    firebase2

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -4,8 +4,9 @@ import React from 'react'
 const Nosotros = () => {
     return (
         <div><h1>Nosotros</h1>
-        <img alt="imagen de bolsita" src="../carrito.png" />
+        <img alt="imagen de bolsita" src="../emi.jpg" style= {{ height: '90vh',width:'100vw' }} />
         </div>
+        
         )
 }
 
