67442056119cbf176443e15a60726675a8dd5db2
franco fazzio <francofazzio09@hotmail.com>
2022-04-25 20:39:15 -0300

    ultima entrega

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-
+import Container from '@mui/material/Container';
 
 const Nosotros = () => {
     return (
-        <div><h1>Nosotros</h1>
+        <Container className='container-general'>
+      <div>
+            <h1>Nosotros</h1>
         <img alt="imagen de bolsita" src="../emi.jpg" style= {{ height: '90vh',width:'100vw' }} />
-        </div>
         
-        )
-}
-
-
+    
+        <section>
+            <h1>"Tienda Online de la tienda de eme - Nuestra esencia"</h1>
+            <h2>"La tienda de eme " realiza accesorios para nenes y nenas.</h2>
+        <p>Somos un emprendimiento que se especializa en accesorios para nenas y nenes, nacida en el año 2020 en la ciudad de Santa Fe capital</p>
+        <p>el objetivo que buscamos desde el comienzo y a largo plazo es muy claro, mantener una optima calidad, ya que es lo primordial para que nuestros clientes nos sigan elgiendo dia a dia</p>
+        <p>Cada pieza es única, hecha a mano cuidando siempre la estética, la calidad y teniendo como principio nuestro amor por los detalles y la felicidad de nuestros niños.</p>
+        <p>Gracias por dejarnos ser parte de sus momentos felices</p>
+        </section>
+        </div>
+     </Container>
+    )
+    }
+    
+    
 export default Nosotros
